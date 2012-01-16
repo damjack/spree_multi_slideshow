5b34e115ef1e6557588e3c3b56191b50e457deef
Damiano <damiano.giacomello@diginess.it>
2012-01-16 15:15:27 +0100

    fix routing admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-      resources :slideshow_types do
-        resources :slides
-      end
+  map.namespace :admin do
+      resources :slideshow_types
+      resources :slides
   end
   
-  resources :slides
-
-  resources :slideshow_types
-
-  # Add your extension routes here
 end
