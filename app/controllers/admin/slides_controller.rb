de79576b81edd34df0f847f8fc3c929b3fc66f2f
Damiano <damiano.giacomello@diginess.it>
2012-01-16 17:33:00 +0100

    remove action from controller admin.

--- app/controllers/admin/slides_controller.rb
+++ app/controllers/admin/slides_controller.rb
@@ -1,7 +1,3 @@
 class Admin::SlidesController < Admin::BaseController
   resource_controller
-
-   update.wants.html { redirect_to collection_url }
-   create.wants.html { redirect_to collection_url }
-   destroy.success.wants.js { render_js_for_destroy }
 end
