4e4db4d3b2b652bdddf73945d1579c6c5d53f631
Damiano <damiano.giacomello@diginess.it>
2012-04-15 12:23:56 +0200

    added s3 storage and fixed migration

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -22,13 +22,44 @@ module Spree
                   :slide => "-gravity center",
                   :custom => "-gravity center"
             }
-
+    
+    after_post_process :find_dimensions
     #process_in_background :image UTILE MA OCCORRE ATTIVARE ANCHE LA GEMMA DELAYED-PAPERCLIP
-
+    
+    # Load S3 settings
+    if (Rails.root.join('config', 's3.yml'))
+      S3_OPTIONS = {
+              :storage => 's3',
+              :s3_credentials => Rails.root.join('config', 's3.yml')
+            }
+    elsif (ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET'])
+      S3_OPTIONS = {
+              :storage => 's3',
+              :s3_credentials => {
+                :access_key_id     => ENV['S3_KEY'],
+                :secret_access_key => ENV['S3_SECRET']
+              },
+              :bucket => ENV['S3_BUCKET']
+            }
+    else
+      S3_OPTIONS = { :storage => 'filesystem' }
+    end
+    
+    attachment_definitions[:image] = (attachment_definitions[:image] || {}).merge(S3_OPTIONS)
+    
     def initialize(*args)
       super(*args)
       last_slide = Slide.last
       self.position = last_slide ? last_slide.position + 1 : 0
+    end
+    
+    def find_dimensions
+      temporary = image.queued_for_write[:original]
+      filename = temporary.path unless temporary.nil?
+      filename = image.path if filename.blank?
+      geometry = Paperclip::Geometry.from_file(filename)
+      self.slide_width  = geometry.width
+      self.slide_height = geometry.height
     end
 
   end
--- db/migrate/20120116081431_create_slideshow_types.rb
+++ db/migrate/20120116081431_create_slideshow_types.rb
@@ -2,11 +2,11 @@ class CreateSlideshowTypes < ActiveRecord::Migration
   def change
     create_table :slideshow_types do |t|
       t.string :category
-      t.boolean :enabled, :default => 0
+      t.boolean :enabled, :default => false
       t.integer :slide_height, :default => 400
       t.integer :slide_width, :default => 900
       t.integer :slide_number, :default => 4
-      t.boolean :enable_navigation, :default => 1
+      t.boolean :enable_navigation, :default => true
       
       t.timestamps
     end
