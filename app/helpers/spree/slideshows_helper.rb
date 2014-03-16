6f30d8c3a4980755f3e6ff5b9a1599cbeb112c81
Damiano <giacomello.damiano@gmail.com>
2014-03-16 22:45:43 +0100

    add insert_carousel_taxon

--- app/helpers/spree/slideshows_helper.rb
+++ app/helpers/spree/slideshows_helper.rb
@@ -65,8 +65,8 @@ module Spree
     end
     
     def insert_carousel(params={})
-      params[:id] ||= "slideshow"
-      params[:class] ||= "first_slideshow"
+      params[:id] ||= "carousel"
+      params[:class] ||= "first_carousel"
       params[:category] ||= "home"
       params[:auto] ||= false
       params[:next_text] ||= '>>'
@@ -80,7 +80,7 @@ module Spree
       params[:pagination_class] ||= ""
       res = []
       
-      res << content_tag(:ul, slide_images(params, @@slideshow), :class => "bxslider #{params[:class]}", :id => params[:id])
+      res << content_tag(:ul, carousel_images(params, @@slideshow), :class => "bxslider #{params[:class]}", :id => params[:id])
       res << "<script type='text/javascript'>
         $(function() {
           $('##{params[:id]}').bxSlider({
@@ -105,6 +105,98 @@ module Spree
         </script>"
       
       res.join.html_safe
+    end
+    
+    def carousel_images(params, slideshow)
+      params[:style] ||= "medium"
+      slides = slideshow.slides.enable.sort_by { |slide| slide.position }
+      
+      slides.map do |slide|
+        content_tag(:li, :class => "item") do
+          divs = []
+          
+          divs << link_to(image_tag(slide.attachment.url(params[:style].to_sym)), (slide.url.blank? ? "javascript: void(0)" : slide.url), { :title => slide.title })          
+          divs.join.html_safe
+        end
+      end.join.html_safe
+    end
+    
+    def insert_carousel_taxon(params={})
+      params[:type] ||= "bxslier"
+      params[:id] ||= "carousel"
+      params[:class] ||= "first_carousel"
+      params[:category] ||= "home"
+      params[:auto] ||= false
+      params[:auto_start] ||= false
+      params[:mode] ||= 'horizontal'
+      params[:enable_navigation] ||= true
+      params[:enable_pagination] ||= false
+      params[:next_text] ||= '>>'
+      params[:prev_text] ||= '<<'
+      params[:next_selector] ||= 'bx-next'
+      params[:prev_selector] ||= 'bx-prev'
+      params[:item_width] ||= 120
+      params[:item_margin] ||= 5
+      @@taxon = Spree::Taxon.find_by_permalink!(params[:taxon])
+      if @@taxon.blank? || (!@@taxon.blank? && @@taxon.children.empty?)
+        return ''
+      end
+      params[:pagination_class] ||= ""
+      res = []
+      
+      if params[:type] == "bxslider"
+        res << content_tag(:ul, carousel_taxon_images(params, @@taxon), :class => "#{params[:class]}", :id => params[:id])
+        res << "<script type='text/javascript'>
+          $(function() {
+            $('##{params[:id]}').bxSlider({
+              auto: #{params[:auto]},
+              adaptiveHeight: true,
+              autoStart: #{params[:auto_start]},
+              mode: '#{params[:mode]}',
+              controls: #{params[:enable_navigation]},
+              nextText: '#{params[:next_text]}',
+              nextSelector: '#{params[:next_selector]}',
+              prevText: '#{params[:prev_text]}',
+              prevSelector: '#{params[:prev_selector]}',
+              speed: 2000,
+              pager: false,
+              infiniteLoop: true,
+              hideControlOnEnd: false,
+              autoHover: true,
+              useCSS: true,
+              pagerSelector: '#{params[:pagination_class]}'
+            });
+          });
+          </script>"
+      else
+        res << content_tag(:div, content_tag(:ul, carousel_taxon_images(params, @@taxon), :class => "slides"), :class => "#{params[:class]}", :id => params[:id])
+        res << "<script type='text/javascript'>
+          $(function() {
+            $('##{params[:id]}').flexslider({
+              itemWidth: #{params[:item_width]},
+              animation: 'slide',
+              animationLoop: false,
+              controlNav: #{params[:enable_pagination]},
+              itemMargin: #{params[:item_margin]}
+            });
+          });
+          </script>"
+      end
+      
+      res.join.html_safe
+    end
+    
+    def carousel_taxon_images(params, taxon)
+      items = taxon.children
+      
+      items.map do |item|
+        content_tag(:li, :class => "item") do
+          divs = []
+          
+          divs << link_to(image_tag(item.icon.url()), seo_url(item), { :title => item.name })
+          divs.join.html_safe
+        end
+      end.join.html_safe
     end
     
   end
